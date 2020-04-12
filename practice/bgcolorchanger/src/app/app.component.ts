03e7c79fa9ed412cb6cbd6cff28320942a53e6ef
Digvijay Singh Rawat <digvijaysinghrawat7@gmail.com>
2020-04-13 02:40:11 +0530

    new

---
+++ practice/bgcolorchanger/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bgcolorchanger';
+
+
+      // var color = '#' + Math.random().toString(16).slice(2, 8);
+      // return color;
+
+      // var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+      // while(color.length < 6) {
+      //     color = "0" + color;
+      // }
+      // return "#" + color;
+
+  }
+
