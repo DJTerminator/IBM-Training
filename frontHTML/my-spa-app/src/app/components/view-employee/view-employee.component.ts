ea1e649717bcc142efffceb52a3c04d6ccf367b3
Digvijay Singh Rawat <digvijaysinghrawat7@gmail.com>
2020-04-15 01:13:24 +0530

    add employee bootstrap

---
+++ frontHTML/my-spa-app/src/app/components/view-employee/view-employee.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+@Component({
+  selector: 'app-view-employee',
+  templateUrl: './view-employee.component.html',
+  styleUrls: ['./view-employee.component.css']
+})
+export class ViewEmployeeComponent implements OnInit {
+
+  employees: Array<any> = []
+  showMessage : boolean = false;
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit(): void {
+    this.employeeService.fetchAllEmployees()
+    .subscribe((res:any)=> {
+      console.log(res);
+      this.employees = res;
+    })
+  }
+
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id)
+    .subscribe((res:any)=> {
+      console.log(res);
+     if(res.status == 200){
+       this.showMessage = true;
+       this.employees = this.employees.filter((employee)=> employee.id!=id)
+     }
+    })
+  }
+}
+
+
---
+++ frontHTML/my-spa-app/src/app/services/employee.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8008/api/employee/'
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) { }
+
+  fetchAllEmployees(){
+    return this.http.get(baseUrl);
+  }
+
+  addEmployee(name: string, salary: number){
+    return this.http.post(baseUrl,
+      {name: name, salary: salary},{observe : 'response'})
+
+  }
+
+  deleteEmployee(id: number){
+    return this.http.delete(baseUrl + id, {observe : 'response'})
+ }
+}
